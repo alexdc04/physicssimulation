e87b7d844ed92fcbf2d6f19a9c54f311ae79aa40
pongcj42 <pongcj@gmail.com>
2025-12-02 17:20:35 -0500

    added debug file for experimentation and renamed to main

---
+++ main_debug.py
@@ -0,0 +1,60 @@
+import pybullet as p
+import time
+import pybullet_data
+from xacrodoc import XacroDoc
+import os
+import torch
+
+def read_xacro(file_name: str) -> str:
+    current_file = file_name
+
+    doc = XacroDoc.from_file(f"{current_file}.urdf.xacro")
+
+    urdf_string = doc.to_urdf_string()
+
+    with open(f"{current_file}.urdf", "w") as f:
+            f.write(urdf_string)
+            
+    return file_name
+
+current_robot=read_xacro("dogv1")
+current_environment="plane"
+
+physicsClient = p.connect(p.GUI) #or p.DIRECT for non-graphical version
+p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
+p.setGravity(0,0,-9.81)
+planeId = p.loadURDF(f"{current_environment}.urdf")
+startPos = [0,0,1.25]
+startOrientation = p.getQuaternionFromEuler([0,0,0])
+agent = p.loadURDF(f"{current_robot}.urdf",startPos, startOrientation)
+p.changeDynamics(bodyUniqueId= agent, linkIndex=-1, lateralFriction=1)
+
+maxForce = 50000
+targetVel=12
+
+f_slider = p.addUserDebugParameter("Force",-20,20,10)
+
+numOfJoints = p.getNumJoints(agent)
+jointIds = range(numOfJoints)
+
+print("here", numOfJoints, jointIds)
+joints_dict={}
+for num in range (numOfJoints):
+    if p.getJointInfo(agent, num)[2] != p.JOINT_FIXED:
+        joints_dict[p.getJointInfo(agent, num)[0]]=(p.addUserDebugParameter(str(p.getJointInfo(agent, num)[1]),-1.5,1.5,0))
+print(joints_dict)
+positions={}
+
+while True:
+    
+    for i in joints_dict.keys():
+        positions[i] = (p.readUserDebugParameter(joints_dict[i]))
+    print(p.getBasePositionAndOrientation(agent))
+    p.setJointMotorControlArray(agent, joints_dict.keys(), controlMode=p.POSITION_CONTROL, targetPositions=positions.values())
+    p.stepSimulation()
+    
+    time.sleep(1./240.)
+    
+    
+
+p.disconnect()
