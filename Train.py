b7d7f2022b245c4a00c8cdae3e1ff0971d472e98
pongcj42 <pongcj@gmail.com>
2026-02-03 20:19:16 -0500

    continued reorganization

--- Train.py
+++ Train.py
@@ -1,7 +1,22 @@
 """
 <Train.py>: Program will train model based on environment given.
 
-<Detailed description/explanation of what the script does and how it works>
+The purpose of this program is to train a model using reinfocement learning. This is accomplished through the use of pybullet and pytorch.
+PyTorch is used to create our neural networks, and PyBullet is a physics simulation that is specialized for reinforcement of robotics.
+
+Basic DQN (Deep Q Nework) algorithm:
+    1. An agent is loaded into some sort of simulation with 2 different neural networks: Target and Policy.
+    2. In the simulation, an agent is observed over an episode (period of time) and evaluated after a training period (set number of episodes). The data collected per episode is state action reward action' (SARS).
+    3. This is a batched training algorithm. A replay memory queue (FIFO) stores n SARS data and the training is as follows:
+        a) Run a 
+    4. We then move back to step 2 adjusting reward and hyperparameters while observing changes.
+
+
+Decision making:
+    In order to prevent model tunnelvision to a particular solution, a greedy epsilon algorithm is used. 
+    This is like a regular greedy algo, prioritizing max reward, while maintaining epsilon randomness. Some random actions are taken in order to find potential innovation.
+    
+
 
 Author:   <Cj Pong/Hood Senior Project Team>
 Created:  <02/03/2025>
@@ -38,16 +53,61 @@ import numpy as np
 from pathlib import Path
 from pybullet_utils import bullet_client as bc
 import msgspec.json
+import json
+from pathlib import Path
+from modules.data_processing import *
+import torch.nn as nn
 
+def initialize(dir_name: str, session_no: int) -> tuple:
+    """Loads session data for a given scenario and session.
 
+    Args:
+        dir_name: Scenario data directory.
+        session_no: Session Number.
+
+    Returns:
+        Hyperparameters - Dict \n
+        Target Network Parameters - Dict \n
+        Policy Network Parameters - Dict \n
+    """
+    data=load_json(dir_name, f'Session_{session_no}')
+    return data["Hyperparameters"], data['Target'], data['Policy']
+
+class NeuralNetwork(nn.Module):
+        def __init__(self, name, num_actions, num_states):
+            super().__init__()
+            self.name=name
+            self.linear_relu_stack=nn.Sequential(
+                nn.Linear(num_states, 128),
+                nn.ReLU(),
+                nn.Linear(128, 128),
+                nn.ReLU(),
+                nn.Linear(128, num_actions),
+            )
+
+        def forward(self, x):
+            x = self.linear_relu_stack(x)
+            return x, torch.argmax(x).int()
+        
+        def save(self, dir: str):
+            torch.save(self.state_dict(), f'{dir}/{self.name}.pth')
 
 if __name__ == "__main__":
-    environment_folder="Basic Walking"
-    session_no=1
-    model=""
     
-    with open(f'{environment_folder}/Session_{session_no}.json', 'rb') as f:
-        data = msgspec.json.load(f)
-        
-    print(data)
+    dir_name='Basic_Walking'
+    session_no=1
+    network_dims=""
+    
+    
+    
+    
+    
+    
+    
+    hyp_params, policy, target = initialize(dir_name, session_no)
+    
+    
+    
+    
+    
 
--- modules/data_processing.py
+++ modules/data_processing.py
@@ -1,13 +1,46 @@
+"""
+<Train.py>: Program will train model based on environment given.
+
+<Detailed description/explanation of what the script does and how it works>
+
+Author:   <Cj Pong/Hood Senior Project Team>
+Created:  <02/03/2025>
+Modified: <02/03/2025>
+Version:  <0.0.0>
+Contact:  pongcj@gmail.com
+
+Dependencies:
+    XacroDoc
+    PyBullet
+    PyTorch -> BulletClient extension
+    Numpy
+    Pandas
+
+Usage:
+    1. First create a session_#.json according to the sample (first 3 sections in JSON).
+    2. Fill out parameters in main section.
+    3. Run program to train data and observe results.
+    4. Save data to retrain next session.
+"""
+
+
 from pathlib import Path
 from xacrodoc import XacroDoc
-from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive
 import json
 import io
 import pickle
 import time
 
 def read_xacro(file_name: str) -> str:
+    """Loads xacro model into string.
+
+    Args:
+        file_name: Name of model.
+
+    Returns:
+        Raw xacro string
+    """
+    
     current_file = file_name
 
     doc = XacroDoc.from_file(f"models/xacro/{current_file}.urdf.xacro")
@@ -19,92 +52,24 @@ def read_xacro(file_name: str) -> str:
             
     return "models/raw/" + file_name + ".urdf"
 
-class DriveConnection():
-    def __init__(self):
-        self.drive=self.connect_to_drive()
-        self.types=json.load(open('mime_types.json'))
-        print("Loading Directories.")
-        self.main_dirs=self.query(conditions=[f"mimeType='{self.types.get('folder')}'"])
-        print("Finished loading directories.\n")
-    
-    def connect_to_drive(self):
-        """Returns connection for a drive connection. Correct yaml settings or secret is required."""
-        gauth = GoogleAuth() 
-        gauth.LocalWebserverAuth()
-        return GoogleDrive(gauth)
+def load_json(dir_name: str, file_name: str) -> dict:
+    """Loads a dict from JSON file.
 
-    def get_dir_id(self, dir: str):
-        return self.main_dirs.get(f'{dir}')
+    Args:
+        dir_name: Name of directory.
+        file_name: Name of file.
+
+    Returns:
+        Returns a dict from given JSON.
+    """
+    try:
+        with open(f'{dir_name}/{file_name}.json', 'r', encoding='utf-8') as f:
+            data = json.load(f)
+    except FileNotFoundError:
+        print("File not found")
+    except json.JSONDecodeError as e:
+        print(f"Invalid JSON: {e}")
     
-    def query(self, dir='root', conditions=[]) -> dict:
-        """
-        Queries database. By default it will give everything in the root dir.\n
-        Conditions should be in the following format;\n
-        "["condition = 'value'", ...]'"\n
-        This returns the requested data as {title:id}
-        """
-        query=''
-        if conditions:
-            for x in conditions:
-                query+= f"and {x}"
-        old=time.time()
-        print(f"Beginning query with extra conditions: {query}")
-        file_list = self.drive.ListFile({'q': f"'{dir}' in parents and trashed=false {query}"}).GetList()
-        print(f"Finished query. Time Elapsed: {time.time()-old} seconds.\n")
-        return {x['title']: x['id'] for x in file_list}
-    
-    def get(self, dir='root', conditions=[]) -> list:
-        """
-        Extension of query. If desired, files are converted from drive to correct format.\n
-        """
-        location=self.get_dir_id(dir)
-        values=self.query(location, conditions)
-        old=time.time()
-        print(f"Beginning conversion for {len(conditions)} files.")
-        out=[]
-        for value in list(values.values()):
-            temp = (self.drive.CreateFile({'id':value}))
-            temp.FetchContent()
-            out.append(pickle.loads(temp.content.read()))
-        print(f"Finished Conversion. Time Elapsed: {time.time()-old} seconds.\n")
-        return out
-    
-    def save(self, data: object, name: str, dir='root'):
-        """ 
-        Saves data to drive. Default directory is root.
-        """
-        location=self.get_dir_id(dir)
-        old=time.time()
-        print(f"Beginning Data Insertion into {dir}")
-        file=self.drive.CreateFile({'title': name, "parents": [{"id": f"{location}"}]})
-        file.content = io.BytesIO(pickle.dumps(data))
-        file.Upload()
-        print(f"Finished Insertion. Time Elapsed: {time.time()-old} seconds.\n")
-        
-    def exists(self, name, dir: str):
-        location=self.get_dir_id(dir)
-        if self.query(dir=f'{location}', conditions=[f"title='{name}_rm'"]):
-            return True
-        else:
-            return False
+    return data
     
 
-def load_session_data(hp_name: str, model_name: str, conn: DriveConnection):
-    out={}
-    out['model_name']=model_name
-    out['hp']=json.load(open(f'Hyperparameters/{hp_name}'))
-    out['replay_memory']=conn.get(dir='Replay_Memory', conditions=[f"title='{model_name}_rm'"])
-    out['policy_net_parameters']=conn.get(dir='DQN_Parameters', conditions=[f"title='{model_name}_pn_weights'"])
-    out['target_net_parameters']=conn.get(dir='DQN_Parameters', conditions=[f"title='{model_name}_tn_weights'"])
-    out['stats']=conn.get(dir='Stats', conditions=[f"title='{model_name}_stats'"])
-    return out
-
-def save_session_data(data: dict, conn: DriveConnection):
-    name=data.get('model_name')
-    conn.save(data.get('replay_memory'), name+'_rm', dir='Replay_Memory')
-    conn.save(data.get('policy_net_parameters'), name+'_pn_weights', dir='DQN_Parameters')
-    conn.save(data.get('target_net_parameters'), name+'_tn_weights', dir='DQN_Parameters')
-
-test=DriveConnection()
-
-print(test.exists('firs', 'Replay_Memory'))
